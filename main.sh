7e54c7272f7598eb7882717a3402a262e1654eae
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-16 13:33:34 +0300

    dynamic branding

--- main.sh
+++ main.sh
@@ -2,6 +2,12 @@
 set -e
 DEBIAN_FRONTEND=noninteractive
 
+# Get logos
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-logo-text-dark.svg -O ./debian/pika-logo-dark.png
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-logo-text.svg -O ./debian/pika-logo-icon.png
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-logo-text-dark.svg -O ./debian/ubuntu-logo-dark.png
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-logo-text.svg -O ./debian/ubuntu-logo-icon.png
+
 # Clone Upstream
 git clone https://gitlab.gnome.org/GNOME/gnome-control-center -b 44.0
 cp -rvf ./debian ./gnome-control-center
